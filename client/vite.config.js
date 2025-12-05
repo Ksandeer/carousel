b16446944cbc6fe269d625fe1a2e74fa51790d7c
karpix25 <nadaraya@gmail.com>
2025-12-05 14:01:01 -0300

    fix: add proxy config and run container as root

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/templates': 'http://localhost:2305',
+      '/1': 'http://localhost:2305',
+      '/check-status': 'http://localhost:2305',
+      '/health': 'http://localhost:2305',
+    },
+  },
 })
